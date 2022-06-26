2be9c7764b4e3b359733c4d920d8fa6c1669712f
Yingying Feng <yfeng59@my.bcit.ca>
2022-06-25 22:24:22 -0700

    implement tail

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function (arr) {
+  return arr.slice(1);
+}
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); 
+assertEqual(result[0], "Lighthouse"); 
+assertEqual(result[1], "Labs");
